89d76eba099cbe96b081b9278bb23899bacdf81a
lislis <mail@lislis.de>
2025-12-10 14:51:21 +0100

    refactor using structs

--- pumps_water-level_nano/pumps_water-level_nano.ino
+++ pumps_water-level_nano/pumps_water-level_nano.ino
@@ -4,42 +4,75 @@
  * two water level sensors, two transistors, two motors/pumps
  */
 
-#define WATER_LEVEL_1 A0
-#define WATER_LEVEL_2 A1 
+#define TIMER_LENGTH 2000
 
-#define POWER_PIN_1 7
-#define POWER_PIN_2 8
+struct Pump {
+  int sensorPin;
+  int powerPin;
+  int switchPin;
+  int sensorReading;
+  bool powerState;
+  bool switchState;
+  unsigned long timerLength;
+  bool isTimerRunning;
+  unsigned long timerStart;
+  unsigned long timerElapsed;
+};
 
-#define SWITCH_1 4
-#define SWITCH_2 5
-
+struct Pump pump1, pump2;
 
 void setup() {
-  pinMode(POWER_PIN_1, OUTPUT);
-  pinMode(POWER_PIN_2, OUTPUT);
-  digitalWrite(POWER_PIN_1, LOW);
-  digitalWrite(POWER_PIN_2, LOW);
+  init_pump(pump1, A0, 7, 4);
+  init_pump(pump2, A1, 8, 5);
+
   Serial.begin(115200);
   Serial.println("setup done");
 }
 
 void loop() {
+  measure_water_level(pump1);
+  measure_water_level(pump2);
+
+  if (pump1.sensorReading > 300) {
+    pump1.switchState = HIGH;
+    digitalWrite(pump1.switchPin, pump1.switchState);
+  } else if (pump1.sensorReading < 200) {
+    pump1.switchState = LOW;
+    digitalWrite(pump1.switchPin, pump1.switchState);
+  }
+
+
   Serial.print(0);
   Serial.print(",");
-  Serial.print(measure_water_level(POWER_PIN_1, WATER_LEVEL_1));
+  Serial.print(pump1.sensorReading);
   Serial.print(",");
-  Serial.print(measure_water_level(POWER_PIN_2, WATER_LEVEL_2));
+  Serial.print(pump2.sensorReading);
   Serial.print(",");
   Serial.println("1023");
   delay(800);
 }
 
-int measure_water_level(int power, int pin) {
-  int val;
-  digitalWrite(power, HIGH);
+void measure_water_level(Pump& p) {
+  digitalWrite(p.powerPin, HIGH);
   delay(10);
-  val = analogRead(pin);
+  p.sensorReading = analogRead(p.sensorPin);
   delay(2);
-  digitalWrite(power, LOW);
-  return val;
+  digitalWrite(p.powerPin, LOW);
 }
+
+void init_pump(Pump& p, int sP, int pP, int swP) {
+  p.sensorPin = sP;
+  p.powerPin = pP;
+  p.switchPin = swP;
+  p.sensorReading = 0;
+  p.powerState = LOW;
+  p.switchState = LOW;
+  p.timerLength = TIMER_LENGTH;
+  p.isTimerRunning = false;
+
+  pinMode(p.powerPin, OUTPUT);
+  pinMode(p.switchPin, OUTPUT);
+  digitalWrite(p.powerPin, LOW);
+  digitalWrite(p.switchPin, LOW);
+}
+
